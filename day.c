98ebf72d6957cbd1f178552ce83edb6ba1453b51
naitik480 <anjitad770@gmail.com>
2025-11-12 16:08:24 +0530

    Add files via upload

---
+++ day.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main (){
+    int day_number;
+    printf("enter Day_number:");
+    scanf("%d",&day_number);
+
+    if(day_number==1){
+        printf("Sunday",day_number);
+
+    }else if(day_number==2){
+        printf("Monday",day_number);
+
+        }else if(day_number==3){
+        printf("Tuesday",day_number);
+
+        }else if(day_number==4){
+        printf("Wednesday",day_number);
+
+        }else if(day_number==5){
+        printf("Thursday",day_number);
+
+        }else if(day_number==6){
+        printf("Friday",day_number);
+
+        }else if(day_number==7){
+        printf("Saturday",day_number);
+
+        }
+
+        return 0;
+
+}
